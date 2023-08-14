e6d910bac36ffd46e4abad67407771a712e0c56d
Juan Pablo Cordero <34517350+jucordero@users.noreply.github.com>
2023-08-14 16:02:56 +0100

    impact data (#19)

---
+++ agrifoodpy_data/impact/__init__.py
@@ -0,0 +1,21 @@
+"""Food module"""
+
+import os
+import xarray as xr
+
+available = ['PN18', 'PN18_FAOSTAT']
+data_dir = os.path.join(os.path.dirname(__file__), 'data/' )
+
+def __getattr__(name):
+    if name not in available:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}.")
+
+    _data_file = f'{data_dir}{name}.nc'
+
+    # If the file contains more than a single dataarray, then it will try
+    # to load it as a dataset
+    try:
+        data = xr.open_dataarray(_data_file)
+    except ValueError:
+        data = xr.open_dataset(_data_file)
+    return data
