f3ac1e14f3295095926ab54372cf29f42ec96c28
jucordero <obejaamarilla@hotmail.com>
2024-02-26 17:43:08 +0000

    Fixed nutrients data to g_nutr / g_food

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ HERE = pathlib.Path(__file__).parent
 
 VERSION = '0.1.0'
 PACKAGE_NAME = 'agrifoodpy_data'
-AUTHOR = 'FixOurFood'
+AUTHOR = 'FixOurFood developers'
 AUTHOR_EMAIL = 'juanpablo.cordero@york.ac.uk'
 URL = 'https://github.com/FixOurFood/agrifoodpy-data'
 
-LICENSE = 'Apache License 2.0'
+LICENSE = 'BSD-3-Clause license'
 DESCRIPTION = 'Prepackaged data for the AgriFoodPy modelling package'
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
@@ -19,6 +19,7 @@ INSTALL_REQUIRES = [
       'pandas',
       'xarray',
       'matplotlib',
+      'netcdf4'
 ]
 
 setup(name=PACKAGE_NAME,
